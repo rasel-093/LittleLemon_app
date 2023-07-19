2b0b57cdd436ab68bd4231ec6b02d87feaa42964
Rasel Rahman <rasel.cse19.ruet@gmail.com>
2023-07-20 01:45:51 +0600

    This is very beginning

---
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.littlelemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.example.littlelemon.ui.theme.LittleLemonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RestaurantName(name = stringResource(id = R.string.title), size = 32)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RestaurantName(name: String, size: Int){
+    Text(
+        text = name,
+        fontSize = size.sp
+    )
+}
+@Preview
+@Composable
+fun RestaurantNameView(){
+    RestaurantName(name = stringResource(id = R.string.title), size = 32)
+}
+
