daaa051d6ee2f04b8b156e3b6fa5077af0c1a38c
Rasel Rahman <rasel.cse19.ruet@gmail.com>
2023-07-23 00:58:56 +0600

    rememberSaveable is used instead of remember inside ItemOrder() function

--- app/src/main/java/com/example/littlelemon/ItemOrder.kt
+++ app/src/main/java/com/example/littlelemon/ItemOrder.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,7 +33,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ItemOrder(){
-    var count by remember {
+    var count by rememberSaveable() {
         mutableStateOf(0)
     }
     Column(
